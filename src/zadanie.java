ad14a665f3d402498a962667d948042bfd45b1a9
vladimir238 <bokovvladimir1@mail.ru>
2022-01-20 11:57:53 +0300

    Second commit

--- src/zadanie.java
+++ src/zadanie.java
@@ -1,12 +1,18 @@
- public class Loader {
+import java.util.Arrays;
+
+class Loader {
     public static void main(String[] args) {
         int value = add (3, 4);
         int result = add(value, 10);
+        System.out.println(result);
+        System.out.println(result);
+        System.out.println(result);
         System.out.println(result);
     }
 
     public static int add(int x, int y) {
         int result = x + y;
         return result;
+
     }
 }
