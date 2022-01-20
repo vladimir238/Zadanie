51b6b399db4a996efa1f75cb3cca0436fdc3c13c
vladimir238 <bokovvladimir1@mail.ru>
2022-01-20 10:20:37 +0300

    Добавил кНичИсходник

---
+++ src/zadanie.java
@@ -0,0 +1,12 @@
+ public class Loader {
+    public static void main(String[] args) {
+        int value = add (3, 4);
+        int result = add(value, 10);
+        System.out.println(result);
+    }
+
+    public static int add(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+}
